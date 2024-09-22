7caf81c6ca52dcd4568b85cd38629cf734c63439
edoole <88608799+edoole@users.noreply.github.com>
2024-09-23 00:52:48 +0800

    diy-part1.sh

--- config/openwrt-main/diy-part1.sh
+++ config/openwrt-main/diy-part1.sh
@@ -8,6 +8,10 @@
 
 # Add a feed source
 # sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
+sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
+sed -i '$a src-git haibo https://github.com/haiibo/openwrt-packages' feeds.conf.default
+sed -i '$a src-git liuran001 https://github.com/liuran001/openwrt-packages' feeds.conf.default
 
 # other
 # rm -rf package/utils/{ucode,fbtest}
